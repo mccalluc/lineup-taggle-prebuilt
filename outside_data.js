afb194a3d5b0417dba61f99b7089281ffd4bd72b
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-05 16:20:12 -0400

    Outside data works

---
+++ outside_data.js
@@ -0,0 +1,8 @@
+var outside_data = [
+  {
+    id:"data",
+    name:"Data",
+    desc:{separator:"\t",primaryKey:"id",columns:[{column:"todo_field",type:"string"}]},
+    url:"data:text/plain;charset=utf-8,id%09todo_field%0D%0A1%09TODO"
+  }
+];
